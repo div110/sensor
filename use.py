c7b98cbe2cc56bba99f39351ba56214aee376621
div110 <davidxhalamka@gmail.com>
2025-07-06 22:29:40 +0200

    mainly contours

--- use.py
+++ use.py
@@ -1,11 +1,30 @@
 #!/bin/python3
 
 import cv2 as cv
+import numpy as np
+
+
 
 def rescale(frame, scale):
     height = int(scale * img.shape[0])
     width = int(scale * img.shape[1])
     return cv.resize(frame,(width,height))
+
+def translate(img, x, y):
+    translation_matrix = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, translation_matrix, dimensions)
+
+def rotate(img, angle, rotation_point=None):
+    (height,width)=img.shape[:2]
+
+    if rotation_point == None:
+        rotation_point = (width//2,height//2)
+
+    rotation_matrix = cv.getRotationMatrix2D(rotation_point, angle, 1.0)
+    dimensions = (width,height)
+
+    return cv.warpAffine(img, rotation_matrix, dimensions)
 
 #reading video
 video = cv.VideoCapture('video.mp4')
@@ -17,9 +36,22 @@ while True:
     if is_okay != True:
         break
 
+
+
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    frame = cv.GaussianBlur(frame,(3,3), cv.BORDER_DEFAULT)
-    #frame = cv.Canny(frame,100,100)
+    frame = cv.GaussianBlur(frame,(7,7), cv.BORDER_DEFAULT)
+    frame = cv.Canny(frame,100,100)
+    #frame = translate(frame, 100,100)
+    #frame = rotate(frame, 90)
+
+    contours, hierarchies = cv.findContours(frame, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+
+    blank = np.zeros(frame.shape,dtype='uint8')
+    cv.drawContours(blank, contours, -1,(255,0,0),3)
+
+    cv.imshow("Contours", blank)
+
 
     cv.imshow("video", frame)
 
